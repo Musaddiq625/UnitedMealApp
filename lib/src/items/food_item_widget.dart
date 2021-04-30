b16c44ebbba27e96be614794958b6afb03220855
Musaddiq625 <musaddiqahmedkhan007@gmail.com>
2021-04-30 16:20:00 +0500

    Update food_item_widget.dart

--- lib/src/items/food_item_widget.dart
+++ lib/src/items/food_item_widget.dart
@@ -9,6 +9,7 @@ class FoodItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 150,
       decoration:
           BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey.withOpacity(.3)))),
       padding: const EdgeInsets.symmetric(vertical: 10),
